33be5865747ab4a48b6cdbc9ef82e1694b9569ec
Ali Ashkani Nia <ashkaninia@gmail.com>
2024-01-15 13:20:44 +0330

    * minor cmake bugfix in exercise1/cmake * Added BoilerPlate for AST Nodes * Implemented Unary operations * Implemented Constant node

--- exercise1/lib/AST.cpp
+++ exercise1/lib/AST.cpp
@@ -1,5 +1,47 @@
 #include "lib/AST.hpp"
 //---------------------------------------------------------------------------
 namespace ast {
+
+UnaryASTNode::UnaryASTNode(std::unique_ptr<ASTNode> ptr): m_ptr{std::move(ptr)}{
+
+}
+
+ASTNode& UnaryASTNode::getInput(){
+    return *m_ptr;
+}
+
+ASTNode* UnaryASTNode::releaseInput(){
+    return m_ptr.release();
+}
+
+ASTNode::Type UnaryPlus::getType() {
+    return Type::UnaryPlus;
+}   
+double UnaryPlus::evaluate(EvaluationContext& ctx) {
+    return this->getInput().evaluate(ctx);
+}
+
+ASTNode::Type UnaryMinus::getType() {
+    return Type::UnaryMinus;
+}   
+double UnaryMinus::evaluate(EvaluationContext& ctx) {
+    return -this->getInput().evaluate(ctx);
+}
+
+Constant::Constant(double value):m_double(value){
+
+}
+double Constant::getValue(){
+    return m_double;
+}
+
+ASTNode::Type Constant::getType() {
+    return Type::Constant;
+}   
+double Constant::evaluate(EvaluationContext& ctx) {
+    (void)ctx;//suppressing compiler unused parameter warnings 
+    return getValue();
+}
+
 } // namespace ast
 //---------------------------------------------------------------------------
--- exercise1/lib/AST.hpp
+++ exercise1/lib/AST.hpp
@@ -2,6 +2,7 @@
 #define H_lib_AST
 //---------------------------------------------------------------------------
 #include <memory>
+#include "EvaluationContext.hpp"
 //---------------------------------------------------------------------------
 namespace ast {
 //---------------------------------------------------------------------------
@@ -20,7 +21,72 @@ class ASTNode {
         Constant,
         Parameter
     };
+    // Every AST node should implement these functions so they are pure virtuals
+    virtual Type  getType() = 0 ;
+    virtual double evaluate(EvaluationContext& ctx) = 0;
+
+    // This is necessary for clean-ups 
+    // A default implementation is enough for the whole hierarchy, as we use smart pointers 
+    virtual ~ASTNode()=default;
 };
+
+class UnaryASTNode : public ASTNode{
+public:
+    UnaryASTNode(std::unique_ptr<ASTNode> ptr);
+    ASTNode& getInput();
+    ASTNode* releaseInput();
+
+protected:
+    std::unique_ptr<ASTNode> m_ptr;
+};
+
+class UnaryPlus: public UnaryASTNode{
+public:
+    //inheriting constructors
+    using UnaryASTNode::UnaryASTNode;
+    Type  getType() override ;
+    double evaluate(EvaluationContext& ctx) override ;
+};
+
+class UnaryMinus: public UnaryASTNode{
+public:
+    //inheriting constructors
+    using UnaryASTNode::UnaryASTNode;
+    Type  getType() override ;
+    double evaluate(EvaluationContext& ctx) override ;
+};
+
+class BinaryASTNode: public ASTNode{
+    
+};
+class Add: public BinaryASTNode{
+
+};
+class Subtract: public BinaryASTNode{
+
+};
+class Multiply: public BinaryASTNode{
+
+};
+class Divide: public BinaryASTNode{
+
+};
+class Power: public BinaryASTNode{
+
+};
+class Constant: public ASTNode{
+public:
+    Constant(double value);
+    double getValue();
+    Type  getType() override ;
+    double evaluate(EvaluationContext& ctx) override ;
+private:
+    double m_double;
+};
+class Parameter: public ASTNode{
+
+};
+
 //---------------------------------------------------------------------------
 } // namespace ast
 //---------------------------------------------------------------------------
